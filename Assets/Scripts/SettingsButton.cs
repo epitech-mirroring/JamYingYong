347590a030fd54c77477ba82794124f72705861c
Aubane <aubane.nourry@epitech.eu>
2023-05-20 00:48:16 +0200

    :sparkles: Add settings menu (sound and music)

---
+++ Assets/Scripts/SettingsButton.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsButton : MonoBehaviour
+{
+    public GameObject settingsPanel;
+    public GameObject playButton;
+    public GameObject quitButton;
+    public GameObject optionButton;
+
+    public void OpenSettings()
+    {
+        Debug.Log("Open Settings");
+        settingsPanel.SetActive(true);
+        playButton.SetActive(false);
+        quitButton.SetActive(false);
+        optionButton.SetActive(false);
+    }
+
+    public void QuitSettings()
+    {
+        Debug.Log("Quit Settings");
+        settingsPanel.SetActive(false);
+        playButton.SetActive(true);
+        quitButton.SetActive(true);
+        optionButton.SetActive(true);
+    }
+}
