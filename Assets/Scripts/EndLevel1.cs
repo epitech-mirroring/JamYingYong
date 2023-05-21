c4c3a6088d3e32c34ba4926b8e7aaf54383d54f9
Thomas <thomas.boue@epitech.eu>
2023-05-21 21:24:36 +0200

    :construction: Add WIP about the end of the first level

---
+++ Assets/Scripts/EndLevel1.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class EndLevel1 : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene("Assets/Scenes/Main.unity");
+    }
+}
